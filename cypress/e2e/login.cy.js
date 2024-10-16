e8977d0b350b51ae37c92566e58ce2c65a2b8faf
Muyundo <muyundobrian@gmail.com>
2024-10-16 14:50:12 +0300

    Initial

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,13 @@
+describe('Log into orage hrm',()=>{
+    it('Login successfully',()=>{
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+        cy.get('.oxd-button').click()
+        cy.url().should('include', '/dashboard')
+    })
+
+
+
+})
