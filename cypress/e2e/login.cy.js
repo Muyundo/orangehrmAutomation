5f6121d347bf62e1a67e5c357f7dcf4f6b524a70
Muyundo <muyundobrian@gmail.com>
2024-10-17 18:20:27 +0300

    updated login and added commands file

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Log into orage hrm',()=>{
     it('Login successfully',()=>{
-        cy.wisit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.login('Admin', 'Admin123')        
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.login('admin', 'admin123')        
         cy.url().should('include', '/dashboard')
     })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,7 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-cypress.commands.add('login', (username, password) => {
+Cypress.Commands.add('login', (username, password) => {
 
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
