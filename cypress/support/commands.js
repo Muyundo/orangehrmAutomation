fde1181cba018b1ea058cf6d3c17a1001f6b5dcc
Muyundo <muyundobrian@gmail.com>
2024-10-17 17:59:27 +0300

    Added commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+cypress.commands.add('login', (username, password) => {
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+    cy.get('.oxd-button').click()
+})
