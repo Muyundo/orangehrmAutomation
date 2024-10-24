6fcaa4a7c4356be1d77fb7ca13467bb44817ccc9
Muyundo <muyundobrian@gmail.com>
2024-10-24 11:57:12 +0300

    updated CI to run in parallel configuration on both Chrome, and Firefox

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Log into orage hrm',()=>{
     it('Login successfully',()=>{
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.login('admin', 'admin123')        
+        cy.login('Admin', 'admin123')        
         cy.url().should('include', '/dashboard')
     })
 
